04b26baf929378be4691d69935b6c1166d4840db
Clay Dunston <dunstontc@gmail.com>
2018-04-08 01:28:03 -0500

    Aaaand now we're modular

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,22 @@
-const gulp = require('gulp');
-const stripJsonComments = require('gulp-strip-json-comments');
-const gulpWatch = require('gulp-watch');
+const gulp   = require('gulp');
+const watch  = require('gulp-watch');
+const merge  = require('gulp-merge-json');
+const json5  = require('gulp-json5-to-json');
 
-gulp.task('default', () =>
-    gulp.src('./src/viml.tmLanguage.json')
-        .pipe(stripJsonComments())
-        .pipe(gulp.dest('syntaxes'))
+gulp.task('default', () => {});
+
+gulp.task('parse-json', () =>
+  gulp.src('./src/partials/*.json5')
+  .pipe(merge({
+    fileName: "viml.tmLanguage.json",
+    json5: true,
+  }))
+  .pipe(json5({
+    beautify: true,
+  }))
+  .pipe(gulp.dest('./syntaxes'))
+);
+
+gulp.task('watch', () =>
+  gulp.watch('./src/**/*.json5', [ 'parse-json' ])
 );
